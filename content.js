25db599b0e7185957a6446044632d4ca10f0aced
Jake Lohman <jakelohman7@gmail.com>
2024-01-04 10:00:01 -0600

    Updating value and toggling, but the original score flickers on pageload

--- content.js
+++ content.js
@@ -1,14 +1,56 @@
-const reviewScoreContainer = document.querySelector("[class^='ScoreCircle']");
-let scoreParagraph = null;
+let originalScore = "?";
+let scoreVisible = false;
 
-if (reviewScoreContainer) {
-  scoreParagraph = reviewScoreContainer.querySelector("p:first-child");
-} else {
-  console.log("Unable to find review score container");
+let scoreElement = null;
+let reviewScoreContainer = null;
+
+main();
+
+function setReviewScoreElement() {
+  reviewScoreContainer = document.querySelector("[class^='ScoreCircle']");
+  if (!reviewScoreContainer) return;
+
+  const scoreParagraph = reviewScoreContainer.querySelector("p:first-child");
+  if (scoreParagraph) {
+    scoreElement = scoreParagraph;
+    debugMessage("Hiding score (display=none)");
+    scoreElement.style.display = "none";
+  }
 }
 
-if (scoreParagraph) {
-  console.log(scoreParagraph.textContent);
-} else if (reviewScoreContainer) {
-  console.log("Unable to find review score")
+function hideScore() {
+  debugMessage("Hiding score");
+  scoreElement.textContent = "—";
+}
+
+function showScore() {
+  debugMessage("Showing score");
+  scoreElement.textContent = originalScore;
+}
+
+function toggleScoreVisibility() {
+  debugMessage("Toggle score clicked");
+  if (scoreVisible) {
+    hideScore();
+  } else {
+    showScore();
+  }
+  scoreVisible = !scoreVisible;
+}
+
+// Custom logger because Pitchfork's console is cluttered with errors/warnings
+function debugMessage(message) {
+  console.log(`\n——— Pitchfork Score Hider: ———\n${message}\n`);
+}
+
+function main() {
+  setReviewScoreElement();
+  if (scoreElement) {
+    originalScore = scoreElement.textContent;
+  } else {
+    debugMessage("Could not locate score element");
+  }
+  hideScore();
+  scoreElement.style.display = "block";
+  reviewScoreContainer.onclick = toggleScoreVisibility;
 }
